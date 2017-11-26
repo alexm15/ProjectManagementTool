ef94bf8eeab806582522282704ab6153122c73b0
alexm15 <alexander.markussen@live.dk>
2017-11-26 18:19:16 +0100

    First working version of program

--- client/js/controllers/mainController.js
+++
@@ -1,5 +0,0 @@
-app.controller('mainController', function($scope, trelloAPI) {
-    $scope.test = "Angular works";
-    $scope.testService = trelloAPI.testService();
-    $scope.runTrelloAPI = trelloAPI.authorize();
-});
