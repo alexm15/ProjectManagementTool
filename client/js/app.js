915ea3d49294b1e2a9ab6290ca18dd703d71386a
alexm15 <alexander.markussen@live.dk>
2018-01-12 18:10:59 +0100

    attempt at angular

--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,21 @@
 var app = angular.module("myApp", []);
 
-app.controller("myController", function($scope) {
+app.controller("myController", ['$scope', '$http', function($scope, $http) {
     $scope.test = "Angular works";
-});
+    
+    $scope.GetTrelloList = function () {
+        $http.get('https://api.trello.com/boards/5a008ddf7f0afee2bb74b510', {
+            headers: {
+                'Access-Control-Allow-Origin': "*"
+            },
+            params: {
+                "key": "6ee5f865fe77804b7e5d6e70bbb46350",
+                "token": "478c9ec0c1c071d81fc489f9c7f1800d36d45e31d9d74e13bfe0583f18f9383c"
+            }
+        }).then(function successCallback(response) {
+            $scope.Details = response;
+        }, function errorCallback(response) {
+            $scope.ResponseDetails = response;
+        });
+    };
+}]);
